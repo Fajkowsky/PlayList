34c1bf209ea268c6eed84747bc24635942b71a43
Fajkowsky <dawid.fajkowski@skygate.pl>
2013-11-23 12:23:44 +0100

    added admin site to backend

--- landingpage/admin.py
+++ landingpage/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from models import Song, SongVoted
 
-# Register your models here.
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('user', 'artist', 'song_name', 'score_plus', 'score_minus')
+
+
+class SongVotedAdmin(admin.ModelAdmin):
+    list_display = ('user', 'song')
+
+admin.site.register(SongVoted, SongVotedAdmin)
+admin.site.register(Song, SongAdmin)
--- main/urls.py
+++ main/urls.py
@@ -4,14 +4,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'landingpage.views.index', name='index'),
     url(r'^frontpage/$', 'landingpage.views.frontpage', name='frontpage'),
     url(r'^register/$', 'landingpage.views.register', name='register'),
     url(r'^addsong/$', 'landingpage.views.addsong', name='addsong'),
     url(r'^mysong/$', 'landingpage.views.mysong', name='mysong'),
     url(r'^logout/$', 'landingpage.views.logouting', name='logouting'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
