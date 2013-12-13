3f2226bdd822008138908957d1e996340c26f31a
Fajkowsky <dawid.fajkowski@gmail.com>
2013-12-13 19:04:48 +0100

    updated dependc

--- setupy.py
+++ setupy.py
@@ -3,7 +3,11 @@ from setuptools import setup, find_packages
 
 install_requires = [
     'Django==1.6.0',
-
+    'dj-database-url==0.2.2'.
+	'dj-static==0.0.5',
+	'gunicorn==18.0',
+	'static==0.4',
+	'wsgiref==0.1.2',
 ]
 
 dependency_links = [
