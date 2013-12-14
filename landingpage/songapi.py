de2bacd0e97a38b20714d67c719a75a17a56c2d9
Fajkowsky <dawid.fajkowski@gmail.com>
2013-12-14 12:33:23 +0100

    fixed problem with utf-8 link

--- landingpage/songapi.py
+++ landingpage/songapi.py
@@ -5,9 +5,9 @@ import urllib2
 
 
 def get_song_link(artist, title):
-    link = 'http://tinysong.com/b/%s+%s?format=json&key=%s' % (
+    link = u'http://tinysong.com/b/%s+%s?format=json&key=%s' % (
         artist, title, settings.TINYSONG_KEY)
-    data = json.load(urllib2.urlopen(link))
+    data = json.load(urllib2.urlopen(link.encode('utf8')))
     if data:
         return data['SongID']
     else:
